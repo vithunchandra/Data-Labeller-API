457a115b98747f56c047dff73eed645d10a4b288
Vithun <vithunchandra@gmail.com>
2023-04-29 15:02:27 +0700

    basic authoriziation

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const {User} = require('../models/index');
+dotenv.config();
+
+const authorization = async (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if(!token){
+        return res.status(401).json({message: "Not Authorized"});
+    }
+    let tokenData = undefined;
+    try{
+        tokenData = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    }catch(error){
+        return res.status(401).json({message: "Invalid JWT Token"});
+    }
+
+    const user = await User.findByPk(token.user_id);
+    if(!user){
+        return res.status(404).json({message: "User not found"});
+    }
+    req.body.user = user;
+    next();
+};
