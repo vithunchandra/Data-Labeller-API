6b35d5187f40e12d01a8086ca86acd76ca7391d3
Valr0c <valent.rocky1302@gmail.com>
2023-06-18 17:52:57 +0700

    upload and get profile picture

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {authorization} = require('../middlewares/middleware');
-const { register , login, topup, topupHistory, retrive_money, update_user,withdrawHistory, userDetail, banUser, getBanUser, removeBan } = require("../controllers/user");
+const { register , login, topup, topupHistory, retrive_money, update_user,withdrawHistory, userDetail, banUser, getBanUser, removeBan, getProfpic } = require("../controllers/user");
 
 router.post("/register", register);
 router.post("/login", login);
@@ -10,6 +10,7 @@ router.get('/topup/history', [authorization], topupHistory);
 router.put("/retrive/:username", retrive_money);
 router.put("/update/:username", update_user);
 router.get('/withdraw/history', [authorization], withdrawHistory);
+router.get('/profpic', [authorization], getProfpic);
 router.get('/:username?', [authorization], userDetail);
 router.post('/ban', [authorization], banUser);
 router.get('/ban/list', [authorization], getBanUser);
